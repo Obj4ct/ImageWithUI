1d8f089ccd2acfb00b4f6eb6700d23e07f361e7f
Zhengtian <71652130+Obj4ct@users.noreply.github.com>
2024-01-13 15:04:43 +0800

    完成了之前 图层混合"正常模式"下的bug 现在功能已经可以正常使用了

--- ImageWithUI/blendwindow.cpp
+++ ImageWithUI/blendwindow.cpp
@@ -9,8 +9,16 @@ BlendWindow::BlendWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->widget_select_blendMode->setVisible(false);
 
+    ui->widget_select_alpha->setVisible(false);
+
+    // 设置滑块的最小值和最大值
+    ui->horizontalSlider_alpha->setMinimum(0);  // 设置最小值
+    ui->horizontalSlider_alpha->setMaximum(10);  // 设置最大值
+
+    currentAlpha = 0; // 或者设置为你期望的初始值
     // 设置下拉框的初始状态
     ui->comboBox_blendMode->addItem("<请选择>");
+    ui->comboBox_blendMode->addItem("正常");
     ui->comboBox_blendMode->addItem("正片叠底");
     ui->comboBox_blendMode->addItem("滤色");
     ui->comboBox_blendMode->addItem("叠加");
@@ -35,7 +43,7 @@ void BlendWindow::on_btn_open_1_clicked() {
     myValue_1 = MYFunction::ReadBMPFile(BMPPath);
 
     originalImageData_1 = myValue_1.imageData;
-    originalImageData_1_temp=originalImageData_1;
+
     bmpImage_1 = QImage(originalImageData_1.data(), myValue_1.bmpInfo.GetWidth(),
                         myValue_1.bmpInfo.GetHeight(), QImage::Format_BGR888);
 
@@ -45,6 +53,7 @@ void BlendWindow::on_btn_open_1_clicked() {
     ui->imageLabel_1->setPixmap(pixmap);
     ui->imageLabel_1->setScaledContents(true);
     selectCount++;
+    qDebug()<<selectCount;
     if(selectCount==2)
     {
         ui->widget_select_blendMode->setVisible(true);
@@ -65,7 +74,6 @@ void BlendWindow::on_btn_open_2_clicked() {
     myValue_2 = MYFunction::ReadBMPFile(BMPPath);
 
     originalImageData_2 = myValue_2.imageData;
-    originalImageData_2_temp=originalImageData_2;
     bmpImage_2 = QImage(originalImageData_2.data(), myValue_2.bmpInfo.GetWidth(),
                         myValue_2.bmpInfo.GetHeight(), QImage::Format_BGR888);
 
@@ -75,6 +83,7 @@ void BlendWindow::on_btn_open_2_clicked() {
     ui->imageLabel_2->setPixmap(pixmap);
     ui->imageLabel_2->setScaledContents(true);
     selectCount++;
+    qDebug()<<selectCount;
     if(selectCount==2)
     {
         ui->widget_select_blendMode->setVisible(true);
@@ -86,6 +95,8 @@ void BlendWindow::BlendImages()
     int32_t width = myValue_1.bmpInfo.GetWidth();
     int32_t height = myValue_1.bmpInfo.GetHeight();
     BlendMode blendMode = currentBlendMode;
+    originalImageData_1=myValue_1.imageData;
+    originalImageData_2=myValue_2.imageData;
     Effect(originalImageData_1, originalImageData_2, width, height, blendMode);
 
 
@@ -113,10 +124,19 @@ void BlendWindow::ShowImage()
 }
 
 void BlendWindow::SwitchBlendMode(uint8_t &destR, uint8_t &destG, uint8_t &destB, uint8_t srcR, uint8_t srcG, uint8_t srcB,
-                                  BlendMode blendMode) {
+                                  BlendMode blendMode,double alpha) {
 
-
+    //originalImageData_1=myValue_1.imageData;
     switch (blendMode) {
+    // 正常模式 在“正常”模式下，“混合色”的显示与不透明度的设置有关。
+    //线性插值
+    case BLEND_NORMAL: {
+
+        destR = static_cast<uint8_t>((1.0 - alpha) * destR + alpha * srcR);
+        destG = static_cast<uint8_t>((1.0 - alpha) * destG + alpha * srcG);
+        destB = static_cast<uint8_t>((1.0 - alpha) * destB + alpha * srcB);
+        break;
+    }
 
         // 正片叠底 其主要特点是以正片作为底图，再将其他图像叠加于其上，并根据叠加图像的颜色来调整底图的 亮度 和 饱和度，产生丰富的合成效果。
         // 这种混合模式的效果是将源像素的颜色应用于目标像素，具有"遮罩"效果，通常用于创建阴影或混合颜色的效果。
@@ -167,7 +187,7 @@ void BlendWindow::SwitchBlendMode(uint8_t &destR, uint8_t &destG, uint8_t &destB
 
 void BlendWindow::Effect(std::vector<uint8_t> &imageData, const std::vector<uint8_t> &effectData, int width, int height,
                          BlendMode blendMode) {
-
+    double alpha=currentAlpha;
     if (imageData.size() != effectData.size()) {
         QMessageBox* myBox = new QMessageBox;
         QPushButton* okBtn = new QPushButton("确定");
@@ -195,31 +215,43 @@ void BlendWindow::Effect(std::vector<uint8_t> &imageData, const std::vector<uint
         uint8_t srcR = effectData[i];
         uint8_t srcG = effectData[i + 1];
         uint8_t srcB = effectData[i + 2];
-        SwitchBlendMode(destR, destG, destB, srcR, srcG, srcB, blendMode);
+        SwitchBlendMode(destR, destG, destB, srcR, srcG, srcB, blendMode,alpha);
     }
 }
 
 
-
 void BlendWindow::on_comboBox_blendMode_currentIndexChanged(int index)
 {
-    originalImageData_1=originalImageData_1_temp;
-    originalImageData_2=originalImageData_2_temp;
+
     if (index == 0) {
         qDebug()<<"current index is 0";
     } else if (index == 1) {
-        // "正片叠底"
-        currentBlendMode = BLEND_MULTIPLY;
+        // "正常"
+        ui->widget_select_alpha->setVisible(true);
+        currentBlendMode = BLEND_NORMAL;
         BlendImages();
     } else if (index == 2) {
-        // "滤色"
-        currentBlendMode = BLEND_SCREEN;
+        // "正片叠底"
+        ui->widget_select_alpha->setVisible(false);
+        currentBlendMode = BLEND_MULTIPLY;
         BlendImages();
     } else if (index == 3) {
+        // "滤色"
+        ui->widget_select_alpha->setVisible(false);
+        currentBlendMode = BLEND_SCREEN;
+        BlendImages();
+    } else if (index == 4) {
         // "叠加"
+        ui->widget_select_alpha->setVisible(false);
         currentBlendMode = BLEND_OVERLAY;
         BlendImages();
     }
+}
+void BlendWindow::on_horizontalSlider_alpha_valueChanged(int value)
+{
+    currentAlpha = static_cast<double>(value) / ui->horizontalSlider_alpha->maximum();
+    qDebug() << "current alpha is " << currentAlpha;
+    BlendImages();
 }
 
 
--- ImageWithUI/blendwindow.h
+++ ImageWithUI/blendwindow.h
@@ -4,6 +4,7 @@
 #include "Debug.h"
 #include "BMPFile.h"
 enum BlendMode {
+    BLEND_NORMAL,     // 正常模式 在“正常”模式下，“混合色”的显示与不透明度的设置有关。
     BLEND_MULTIPLY,   // 正片叠底 其主要特点是以正片作为底图，再将其他图像叠加于其上，并根据叠加图像的颜色来调整底图的亮度和饱和度，产生丰富的合成效果。
     BLEND_SCREEN,     // 滤色 ps中滤色的作用结果和正片叠底刚好相反，它是将两个颜色的互补色的像素值相乘，然后除以255得到的最终色的像素值。通常执行滤色模式后的颜色都较浅。
     BLEND_OVERLAY,    // 叠加 　“叠加”模式把图像的“基色”颜色与“混合色”颜色相混合产生一种中间色。
@@ -26,7 +27,7 @@ private slots:
     void on_btn_open_2_clicked();
 
     void on_comboBox_blendMode_currentIndexChanged(int index);
-
+    void on_horizontalSlider_alpha_valueChanged(int value);
 
 private:
     Ui::BlendWindow *ui;
@@ -42,13 +43,13 @@ private:
     static int selectCount;
     //下拉框选项
     BlendMode currentBlendMode;
-
+    double currentAlpha;
     //first image data
     std::vector<uint8_t> originalImageData_1;
-    std::vector<uint8_t> originalImageData_1_temp;
+
     //seconde image data
     std::vector<uint8_t> originalImageData_2;
-    std::vector<uint8_t> originalImageData_2_temp;
+
     std::vector<uint8_t> blendedImageData;
 
 
@@ -56,7 +57,7 @@ private:
 private:
     void SwitchBlendMode(uint8_t &destR, uint8_t &destG, uint8_t &destB,
                          uint8_t srcR, uint8_t srcG, uint8_t srcB,
-                         BlendMode blendMode);
+                         BlendMode blendMode,double alpha);
 
     void Effect(std::vector<uint8_t> &imageData,
                 const std::vector<uint8_t> &effectData,
